8516c8ee7f616b821fabf49b8fc0274679b08c49
Jordon Cotton <s188044@students.aie.edu>
2019-05-13 18:07:00 -0500

    Functions to start on my seek behaviour

---
+++ Assets/Scenes/AgentBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject tar;
+    public Transform pos;
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
---
+++ Assets/Scenes/SeekBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekBehaviour : MonoBehaviour
+{
+    [SerializeField]
+        public float MaxVelocity = 25;
+        public float Mass = 0;
+        public float MaxForce = 15;
+
+        private Vector3 velocity;
+        public Transform target;
+ 
+	// Use this for initialization
+	void Start ()
+    {
+        velocity = Vector3.zero;
+	}
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        var desiredVelocity = target.transform.position - transform.position;
+        desiredVelocity = desiredVelocity.normalized * MaxVelocity;
+
+        var steering = desiredVelocity - velocity;
+        steering = Vector3.ClampMagnitude(steering, MaxForce);
+        steering /= Mass;
+
+        velocity = Vector3.ClampMagnitude(velocity + steering, MaxVelocity);
+        transform.position += velocity * Time.deltaTime;
+        transform.forward = velocity.normalized;
+	}
+}
